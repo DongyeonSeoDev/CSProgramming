7fb1bcd92ab5fb66171e734c164a6014f276edef
sdy362 <seodye04@gmail.com>
2021-04-13 22:56:13 +0900

    기능추가: 곱셈을 출력하는 프로그램 추가

---
+++ Code/01. Multiplication/Multiplication/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multiplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n곱셈을 출력하는 코드\n");
+
+
+            for (int x = 1; x <= 9; x++)
+            {
+                for (int i = 1; i <= 9; i++)
+                {
+                    Console.WriteLine(x + " * " + i + " = " + x * i);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
