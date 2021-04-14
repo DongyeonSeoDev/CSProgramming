a622b891d606975892d3313b586bb0543af1605d
sdy362 <seodye04@gmail.com>
2021-04-14 22:19:30 +0900

    기능추가: 피라미드를 출력하는 프로그램 추가

---
+++ Code/02. PyramidOutput/PyramidOutput/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PyramidOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int length = 10;
+
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < length - i - 1; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 0; j < i * 2 + 1; j++)
+                {
+                    Console.Write("*");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
