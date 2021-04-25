dca0638b7cb4a097eaaa12b6acc49fe99c04c351
sdy362 <seodye04@gmail.com>
2021-04-26 00:25:13 +0900

    기능추가: 읽고 말하기 수열을 출력하는 프로그램 추가

---
+++ Code/04. LookAndSaySequence/LookAndSaySequence/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace LookAndSaySequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("읽고 말하기 수열\n");
+
+            string start = "1";
+            int count = 0;
+            string end = "";
+            char same = ' ';
+
+            for (int i = 0; i < 15; i++)
+            {
+                WriteLine($"{i + 1}번째 수열: {start}");
+                count = 0;
+                same = start[0];
+                end = "";
+
+                for (int j = 0; j < start.Length; j++)
+                {
+                    if (start[j] == same)
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        end = end + same + count;
+                        same = start[j];
+                        count = 1;
+                    }
+                }
+
+                end = end + same + count;
+                start = end;
+            }
+
+            WriteLine();
+        }
+    }
+}
