c39c65002f33d94034f34f9bd14ef99bf95c771a
sdy362 <seodye04@gmail.com>
2021-05-25 00:25:15 +0900

    기능추가: 제곱 계산기 프로그램 추가

---
+++ Code/05. SquareCalculator/SquareCalculator/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace SquareCalculator
+{
+    class Program
+    {
+        static void Square(int num, out int output)
+        {
+            output = num * num;
+        }
+
+        static void Main(string[] args)
+        {
+            WriteLine("제곱 계산기");
+            Write("숫자 입력: ");
+            int output;
+            bool result = int.TryParse(ReadLine(), out output);
+            if (result && output <= 46340)
+            {
+                Square(output, out output);
+                WriteLine("결과: " + string.Format("{0:#,###}", output));
+            }
+            else
+            {
+                WriteLine("잘못된 입력 입니다.");
+            }
+        }
+    }
+}
