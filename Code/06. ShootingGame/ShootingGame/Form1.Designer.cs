25d1075d112e7a0c0641abb42ddc475d99bb3391
sdy362 <seodye04@gmail.com>
2021-07-08 22:50:51 +0900

    기능추가: 슈팅게임에서 플레이어 움직임 추가

---
+++ Code/06. ShootingGame/ShootingGame/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace ShootingGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Player
+            // 
+            this.Player.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Player.Location = new System.Drawing.Point(215, 757);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(50, 50);
+            this.Player.TabIndex = 0;
+            this.Player.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 16;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(482, 856);
+            this.Controls.Add(this.Player);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Code/06. ShootingGame/ShootingGame/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ShootingGame
+{
+    public partial class Form1 : Form
+    {
+        private int speed = 7;
+
+        /// <summary>
+        /// 0: Right, 1: Left, 2: Up, 3: Down
+        /// </summary>
+        private bool[] isMove = new bool[4] { false, false, false, false };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Right: isMove[0] = true; return;
+                case Keys.Left: isMove[1] = true; return;
+                case Keys.Up: isMove[2] = true; return;
+                case Keys.Down: isMove[3] = true; return;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode)
+            {
+                case Keys.Right: isMove[0] = false; return;
+                case Keys.Left: isMove[1] = false; return;
+                case Keys.Up: isMove[2] = false; return;
+                case Keys.Down: isMove[3] = false; return;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (isMove[0])
+            {
+                MoveRight();
+            }
+
+            if (isMove[1])
+            {
+                MoveLeft();
+            }
+
+            if (isMove[2])
+            {
+                MoveUp();
+            }
+
+            if (isMove[3])
+            {
+                MoveDown();
+            }
+        }
+
+        private void MoveRight()
+        {
+            if (Player.Location.X > 430) return;
+
+            Player.Location = new System.Drawing.Point(Player.Location.X + speed, Player.Location.Y);
+            Invalidate();
+        }
+
+        private void MoveLeft()
+        {
+            if (Player.Location.X < 2) return;
+
+            Player.Location = new System.Drawing.Point(Player.Location.X - speed, Player.Location.Y);
+            Invalidate();
+        }
+
+        private void MoveUp()
+        {
+            if (Player.Location.Y < 2) return;
+
+            Player.Location = new System.Drawing.Point(Player.Location.X, Player.Location.Y - speed);
+            Invalidate();
+        }
+
+        private void MoveDown()
+        {
+            if (Player.Location.Y > 800) return;
+
+            Player.Location = new System.Drawing.Point(Player.Location.X, Player.Location.Y + speed);
+            Invalidate();
+        }
+    }
+}
