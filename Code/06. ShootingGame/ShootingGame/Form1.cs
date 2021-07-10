87fe42db3e9e4cb8ec785585ad09bfaa7219082f
sdy362 <seodye04@gmail.com>
2021-07-11 00:10:29 +0900

    버그수정: 슈팅게임에서 디자인 창이 안나오는 버그 수정

--- Code/06. ShootingGame/ShootingGame/Form1.cs
+++ Code/06. ShootingGame/ShootingGame/Form1.cs
@@ -8,18 +8,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-class Position
-{
-    public int x;
-    public int y;
-
-    public Position(int x, int y)
-    {
-        this.x = x;
-        this.y = y;
-    }
-}
-
 namespace ShootingGame
 {
     public partial class Form1 : Form
